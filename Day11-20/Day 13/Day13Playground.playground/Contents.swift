5dc77ccc7968cd38d3d3661f13a6204d254ee8b5
bhishaksanyal <sanyalbishak93@gmail.com>
2022-11-26 20:21:23 +0530

    Day 13 completed

---
+++ Day11-20/Day 13/Day13Playground.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+// Swift review, day one
+
+// Variables and constants
+//let name = "Alex"
+var name = "Alex"
+name = "Paul"
+
+// Datatypes
+let Inte: Int = 25
+let str: String = "Monkey"
+let flo: Float = 24.224
+let doub: Double = 034930490.3444334
+let boo: Bool = false
+
+print(2 + 2 / 2 * 2)
